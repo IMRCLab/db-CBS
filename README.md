cfcca07bd5aabe620368ddd983df450e0ef25cb0
Wolfgang Hoenig <hoenig@tu-berlin.de>
2023-08-16 20:47:46 +0200

    generalize motion primitive location

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # db-CBS
 
+## Get primitives
+
+The primitives are on the TUB cloud. The easiest is to use a symlink:
+
+```
+ln -s /home/whoenig/tubCloud/projects/db-cbs/motions motions
+```
+
 ## Building
 
 Tested on Ubuntu 22.04.
--- scripts/main_dbcbs.py
+++ scripts/main_dbcbs.py
@@ -15,11 +15,10 @@ sys.path.append(os.getcwd())
 
 def run_dbcbs(filename_env, folder, timelimit, cfg):
     with tempfile.TemporaryDirectory() as tmpdirname:
-        p = Path("../results/")
-        filename_motions = p / "dbg/motions.msgpack"
+        filename_motions = Path("..") / "motions" / "dbg_motions.msgpack"
+        print(filename_env)
         filename_stats = "{}/stats.yaml".format(folder)
         start = time.time()
-        dt = 0.1
         duration_dbcbs = 0
         with open(filename_stats, 'w') as stats:
             stats.write("stats:\n")
@@ -28,12 +27,15 @@ def run_dbcbs(filename_env, folder, timelimit, cfg):
             filename_result_dbcbs_joint = Path(folder) / "dbcbs_joint.yaml"
             filename_result_dbcbs_opt = Path(folder) / "result_dbcbs_opt.yaml"
             t_dbcbs_start = time.time()
-            result = subprocess.run(["./db_cbs", 
+
+            cmd = ["./db_cbs", 
                 "-i", filename_env,
                 "-m", filename_motions,
                 "-o", filename_result_dbcbs,
                 "--joint", filename_result_dbcbs_joint,
-                "--opt", filename_result_dbcbs_opt])
+                "--opt", filename_result_dbcbs_opt]
+            print(subprocess.list2cmdline(cmd))
+            result = subprocess.run(cmd)
             t_dbcbs_stop = time.time()
             duration_dbcbs += t_dbcbs_stop - t_dbcbs_start
             if result.returncode != 0:
