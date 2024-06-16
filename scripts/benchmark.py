3e52425ba8b59a706a9531504a2f79749563d3af
akmaralAW <moldagalieva@tu-berlin.de>
2024-06-16 16:03:17 +0200

    dynoplan merge changes, run benchmark with heuristic, constraint time checked

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -117,9 +117,9 @@ def execute_task(task: ExecutionTask):
 			print("WARNING: CHECKER FAILED -> DELETING stats!")
 			(result_folder / "stats.yaml").unlink(missing_ok=True)
 
-	# vis_script = scripts_path / "visualize.py"
-	# for file in visualize_files:
-	# 	run_visualize(vis_script, env, result_folder / file)
+	vis_script = scripts_path / "visualize.py"
+	for file in visualize_files:
+		run_visualize(vis_script, env, result_folder / file)
 	
 	# search_viz_script = scripts_path / "visualize_search.py"
 	# if(len(search_plot_files) > 0):
@@ -133,16 +133,16 @@ def main():
 		# # 1 robot cases
 		"swap1_unicycle",
 		"swap1_unicycle_sphere",
-		# "swap1_trailer",
+		"swap1_trailer",
 		"swap1_unicycle2",
 		"swap1_double_integrator",
 		# 2 robot cases
 		"swap2_unicycle",
 		"swap2_unicycle_sphere",
 		"swap2_double_integrator",
-		# "swap2_trailer",
+		"swap2_trailer",
 		"swap2_unicycle2",
-		# "swap2_hetero",
+		"swap2_hetero",
 		# # "makespan_vs_soc_1",
 		# # "makespan_vs_soc_0",
 		"alcove_unicycle",
@@ -153,13 +153,13 @@ def main():
 		"swap3_unicycle",
 		"swap3_unicycle_sphere",
 		"swap3_double_integrator",
-		# "swap3_trailer",
+		"swap3_trailer",
 		"swap3_unicycle2",
 		# 4 robot cases
 		"swap4_unicycle",
 		"swap4_unicycle_sphere",
 		"swap4_double_integrator",
-		# "swap4_trailer",
+		"swap4_trailer",
 		"swap4_unicycle2",
 
 		# special test cases
@@ -185,10 +185,10 @@ def main():
 	]
 
 	# add random cases
-	# for kind in ["unicycle_sphere", "hetero"]:
-	# 	for n in [2,4,8]:
-	# 		for k in range(10):
-	# 			instances.append("gen_p10_n{}_{}_{}".format(n,k, kind))
+	for kind in ["unicycle_sphere", "hetero"]:
+		for n in [2,4,8]:
+			for k in range(10):
+				instances.append("gen_p10_n{}_{}_{}".format(n,k, kind))
 
 
 	algs = [
