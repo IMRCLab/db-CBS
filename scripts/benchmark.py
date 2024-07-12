5ba6c3c87dc1bb22a2458da688d7aab43035e83d
akmaralAW <moldagalieva@tu-berlin.de>
2024-07-12 14:52:04 +0200

    benchmark the sequential low-level heuristic

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -122,8 +122,8 @@ def execute_task(task: ExecutionTask):
 	else:
 		vis_script = scripts_path / "visualize.py"
 
-	for file in visualize_files:
-		run_visualize(vis_script, env, result_folder / file)
+	# for file in visualize_files:
+		# run_visualize(vis_script, env, result_folder / file)
 	
 	# search_viz_script = scripts_path / "visualize_search.py"
 	# if(len(search_plot_files) > 0):
@@ -191,8 +191,8 @@ def main():
 		"drone10c",
 		"drone12c",
 		"drone16c",
-		"drone24c",
-		"drone32c",
+		# "drone24c",
+		# "drone32c",
 	]
 
 	# # add random cases
