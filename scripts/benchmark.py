646b033561b12880e9e95eb0421029cbc5b40000
akmaralAW <moldagalieva@tu-berlin.de>
2024-10-03 15:04:09 +0200

    fa with +-10 g bounds, benchmark 1-16

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -184,13 +184,13 @@ def main():
 		# "window4_demo",
 
 		# 3D scenarios with octomap
-		# "drone1c",
-		# "drone2c",
-		# "drone4c",
-		# "drone8c",
+		"drone1c",
+		"drone2c",
+		"drone4c",
+		"drone8c",
 		"drone10c",
 		"drone12c",
-		# "drone16c",
+		"drone16c",
 		# "drone24c",
 		# "drone32c",
 	]
@@ -210,7 +210,7 @@ def main():
 		"db-ecbs",
 	]
 	trials = 1
-	timelimit = 20*60
+	timelimit = 10*60
 
 	tasks = []
 	for instance in instances:
@@ -228,21 +228,20 @@ def main():
 		for task in tasks:
 			execute_task(task)
 	
-	# export_table_txt(instances, algs)
-	run_benchmark_stats(instances, algs, trials, timelimit)
-	write_table(instances, algs, Path("../results"), "table.pdf", trials, timelimit)
+	# run_benchmark_stats(instances, algs, trials, timelimit)
+	# write_table(instances, algs, Path("../results"), "table.pdf", trials, timelimit)
 
-	subprocess.run(
-		['pdftk',
-		 Path("../results") / 'table.pdf',
-		 Path("../results") / 'stats.pdf',
-		 'cat', 'output',
-		 Path("../results") / 'results.pdf'
-		]
-	)
+	# subprocess.run(
+		# ['pdftk',
+		#  Path("../results") / 'table.pdf',
+		#  Path("../results") / 'stats.pdf',
+		#  'cat', 'output',
+		#  Path("../results") / 'results.pdf'
+		# ]
+	# )
 	# delete temp files
-	(Path("../results") / 'table.pdf').unlink()
-	(Path("../results") / 'stats.pdf').unlink()
+	# (Path("../results") / 'table.pdf').unlink()
+	# (Path("../results") / 'stats.pdf').unlink()
 
 	# paper_tables.write_table1(trials, timelimit)
 	# paper_tables.write_table2(trials, timelimit)
