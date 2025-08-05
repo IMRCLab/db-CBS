f73a7449d4c358437ddba6680fb8cad8e1a8d1ef
Khaledwahba1994 <Khaled.hassan1994@gmail.com>
2025-08-05 11:21:58 +0200

    some commands for opt

---
+++ mujoco_cmds.sh
@@ -0,0 +1,13 @@
+./dynoplan/main_simulate_mujoco --init_file ../mj_2window_initguess.yaml --env_file ../dynoplan/dynobench/envs/mujoco/simulate_2robots_window.yaml --results_file ../result.yaml --models_base_path ../dynoplan/dynobench/models/
+
+ 
+./dynoplan/main_simulate_mujoco --init_file ../mj_2forest_initguess.yaml --env_file ../dynoplan/dynobench/envs/mujoco/simulate_2robots_forest.yaml --results_file ../result.yaml --models_base_path ../dynoplan/dynobench/models/
+
+./dynoplan/main_simulate_mujoco --init_file ../mj_3window_initguess.yaml --env_file ../dynoplan/dynobench/envs/mujoco/simulate_3robots_window.yaml --results_file ../result.yaml --models_base_path ../dynoplan/dynobench/models/
+
+
+# empty
+./dynoplan/main_simulate_mujoco --init_file ../mj_2empty_initguess.yaml --env_file ../dynoplan/dynobench/envs/mujoco/simulate_2robots_empty.yaml --results_file ../result_empty.yaml --models_base_path ../dynoplan/dynobench/models/
+
+
+./dynoplan/main_simulate_mujoco --init_file ../mj_1empty_initguess.yaml --env_file ../dynoplan/dynobench/envs/mujoco/simulate_1robots_empty.yaml --results_file ../result_1empty.yaml --models_base_path ../dynoplan/dynobench/models/
